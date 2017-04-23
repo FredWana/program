f99b94f715b54e913be37424df1ba9ec0d13ad69
FredWana <blue768lk@gmail.com>
2017-04-24 00:39:20 +0200

    Update ucCaptcha.cs

--- ucCaptcha.cs
+++ ucCaptcha.cs
@@ -1,12 +1,36 @@
 using System;
+using System.IO;
+using System.Text;
 using System.Drawing;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using General.Librerias.EntidadesNegocio;  
 
 namespace General.Librerias.CodigoUsuario
 {
-  class ucCaptcha
+  public class ucCaptcha
   {
-  
+    private static char generarCaracterAzar()
+    {
+      Random oAzar = new Random();
+      int n = oAzar.Next(26)+65;
+      char c=(char)n;
+      return c;
+    }
+    
+    public static beCaptcha CrearCaptcha()
+    {
+      beCaptcha obeCaptcha = new beCaptcha();
+      StringBuilder sb = new StringBuilder();
+      foreach(int i=0;i<5;i++)
+      {
+        sb.Append(generarCaracterAzar());
+      }
+      Rectangle rec=new Rectangle(0,0,200,8);
+      Bitmap bmp =new Bitmap(200,80);
+      Grafics grafico = Grafics.FromImage(bmp);
+      grafico
+      return obeCaptcha;
+    }
   }
 }
