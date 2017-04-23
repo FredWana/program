e18a9852d12cbacf172dfe179faae0f5864328a8
FredWana <blue768lk@gmail.com>
2017-04-24 00:23:49 +0200

    Create beCaptcha.cs

---
+++ beCaptcha.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collection.Generic;
+
+namespace General.Librerias.EntidadesNegocio
+{
+  public class beCaptcha 
+  {
+      public string Codigo{get;set}
+      public byte[] Imagen {get;set}
+  }
+
+}
